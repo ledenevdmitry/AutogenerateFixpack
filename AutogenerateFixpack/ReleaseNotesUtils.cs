209068fd2f64580bf455dc4ae311b37d17bdb44a
dledenev <dimzan18@yandex.ru>
2019-08-02 10:46:19 +0300

    Добавление пререквизитов

--- AutogenerateFixpack/ReleaseNotesUtils.cs
+++ AutogenerateFixpack/ReleaseNotesUtils.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AutogenerateFixpack
 {
@@ -12,16 +13,16 @@ namespace AutogenerateFixpack
     {
         static Microsoft.Office.Interop.Word.Application wordApp;
 
-        private static void SetFixpackName(Document adm_release_notes, DirectoryInfo fixpackDir)
+        private static void ReplaceWithStringValue(Document admReleaseNotes, string findWhat, string ReplaceWith)
         {
             object missing = System.Reflection.Missing.Value;
 
-            Range range = adm_release_notes.Content;
+            Range range = admReleaseNotes.Content;
             Find find = range.Find;
-            find.Text = "FIXPACK_NAME";
+            find.Text = findWhat;
             find.ClearFormatting();
             find.Replacement.ClearFormatting();
-            find.Replacement.Text = fixpackDir.Name;
+            find.Replacement.Text = ReplaceWith;
 
             object replaceAll = WdReplace.wdReplaceAll;
 
@@ -30,33 +31,72 @@ namespace AutogenerateFixpack
                 ref replaceAll, ref missing, ref missing, ref missing, ref missing);
         }
 
+        private static void SetFixpackName(Document admReleaseNotes, DirectoryInfo fixpackDir)
+        {
+            ReplaceWithStringValue(admReleaseNotes, "FIXPACK_NAME", fixpackDir.Name);
+        }
+
+        private static void SetDate(Document admReleaseNotes, DirectoryInfo fixpackDir)
+        {
+            ReplaceWithStringValue(admReleaseNotes, "CREATE_DATE", DateTime.Now.ToString("dd.MM.yyyy"));
+        }
+
+        private static void SetPrerequisites(Document admReleaseNotes, Document devReleaseNotes, bool isLast)
+        {
+            var prereqCell = devReleaseNotes.Tables[1].Cell(6, 1);
+            ReplaceWithStringValue(admReleaseNotes, "PREREQUISITE",
+                prereqCell.Range.Text + (isLast ? "" : Environment.NewLine + "PREREQUISITE"));
+        }
+
         public static void GenerateReleaseNotes(DirectoryInfo fixpackDir)
         {
-            if(wordApp == null)
-               wordApp = new Application();
+            if (wordApp == null)
+                wordApp = new Microsoft.Office.Interop.Word.Application();
 
-            foreach(DirectoryInfo subDir in fixpackDir.EnumerateDirectories("*", SearchOption.TopDirectoryOnly))
+            FileInfo releaseNotesFile = new FileInfo(Path.Combine(fixpackDir.FullName, "release_notes.docx"));
+            FileInfo releaseNotesFileSEED = new FileInfo(Path.Combine(fixpackDir.FullName, "Release_Notes_SEED.docx"));
+
+            DialogResult result = DialogResult.None;
+
+            if (File.Exists(releaseNotesFile.FullName))
             {
-                Document dev_release_notes = wordApp.Documents.Open(Path.Combine(subDir.FullName, "release_notes.docx"), System.Type.Missing, true);
-                /*
-                //var cell1 = doc.Tables[0].Cell(0, 0);
-                var table = dev_release_notes.Tables[1];
-                var cell2 = table.Cell(2, 1);
-                var cell3 = table.Cell(6, 1);
-                var cell4 = table.Cell(8, 1);
-                var cell5 = table.Cell(10, 1);
-                var cell6 = table.Cell(12, 1);
-                var cell7 = table.Cell(14, 2);
-                var cell8 = table.Cell(15, 2);
-                var cell9 = table.Cell(16, 2);
+                result = MessageBox.Show("Файл release_notes.docx существует. Пересоздать?", "Предупреждение", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
 
-                cell2.Range.Copy();
-                */
+                if (result == DialogResult.Yes)
+                {
+                    File.SetAttributes(releaseNotesFile.FullName, FileAttributes.Normal);
+                    releaseNotesFile.Delete();
+                    releaseNotesFileSEED.CopyTo(releaseNotesFile.FullName);
+                }
+            }
 
-                Document adm_release_notes = wordApp.Documents.Open(Path.Combine(fixpackDir.FullName, "release_notes.docx"), System.Type.Missing, true);
+            if(result != DialogResult.Cancel)
+            {
+                Document admReleaseNotes = wordApp.Documents.Open(releaseNotesFile.FullName, System.Type.Missing, true);
 
-                //adm_release_notes.Content.Paste();
-                adm_release_notes.Save();
+                foreach (DirectoryInfo subDir in fixpackDir.EnumerateDirectories("*", SearchOption.TopDirectoryOnly))
+                {
+                    Document devReleaseNotes = wordApp.Documents.Open(Path.Combine(subDir.FullName, "release_notes.docx"), System.Type.Missing, true);
+                    /*
+                    //var cell1 = doc.Tables[0].Cell(0, 0);
+                    var table = dev_release_notes.Tables[1];
+                    var cell2 = table.Cell(2, 1);
+                    var cell3 = table.Cell(6, 1);
+                    var cell4 = table.Cell(8, 1);
+                    var cell5 = table.Cell(10, 1);
+                    var cell6 = table.Cell(12, 1);
+                    var cell7 = table.Cell(14, 2);
+                    var cell8 = table.Cell(15, 2);
+                    var cell9 = table.Cell(16, 2);
+
+                    cell2.Range.Copy();
+                    */
+
+
+                    //adm_release_notes.Content.Paste();
+                    SetPrerequisites(admReleaseNotes, devReleaseNotes, true);
+                    admReleaseNotes.Save();
+                }
             }
 
         }
